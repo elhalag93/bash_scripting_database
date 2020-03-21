b5da9c8b8790edddcd319235dc887d324afcce60
mohamed gamal elhalag <46610094+elhalag93@users.noreply.github.com>
2020-03-22 01:13:30 +0200

    Update view.sh

--- bash_project/view.sh
+++ bash_project/view.sh
@@ -6,8 +6,11 @@ read table
 
 #cd ./Database/$table
 
-awk -F: '{print $0}' ./RDBMS/database/$data_base/$table/Header
+# awk -F: '{print $0}' ./RDBMS/database/$data_base/$table/Header
 
-awk -F: '{print $0}' ./RDBMS/database/$data_base/$table/data
+# awk -F: '{print $0}' ./RDBMS/database/$data_base/$table/data
+
+awk -F: '{print $1"\t"$2"\t"$3"\t"$4"\t"$5"\t"$6"\t"$7}' ./RDBMS/database/$data_base/$table/Header
+awk -F: '{print $1"\t"$2"\t"$3"\t"$4"\t"$5"\t"$6"\t"$7}' ./RDBMS/database/$data_base/$table/data
 
 
