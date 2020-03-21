a1a8e7ae52287b87d8d566ffe634f89c8808cac6
mohamed gamal elhalag <46610094+elhalag93@users.noreply.github.com>
2020-03-22 01:08:07 +0200

    Update search.sh

--- bash_project/search.sh
+++ bash_project/search.sh
@@ -3,6 +3,6 @@ echo "Enter the name of table u want to search in: "
 		echo "enter the column name and value"
 		read value
 
-awk -F, '{print $1"\t"$2"\t"$3}' ./RDBMS/database/$data_base/$table/Header
-awk -F, -vX="$name" '{if( $2 == X){print $1"\t"$2"\t"$3}}' ./RDBMS/database/$data_base/$table/data
-
+awk -F, '{print $1"\t"$2"\t"$3"\t"$4"\t"$5"\t"$6"\t"$7}' ./RDBMS/database/$data_base/$table/Header
+awk -F, -vX="$name" '{if( $2 == X){print $1"\t"$2"\t"$3"\t"$4"\t"$5"\t"$6"\t"$7}}' ./RDBMS/database/$data_base/$table/data
+ ./main_base.sh
