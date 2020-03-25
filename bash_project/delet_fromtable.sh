31bff5b826a758d34e64b0caf548320d2110bc0a
mohamed gamal elhalag <46610094+elhalag93@users.noreply.github.com>
2020-03-26 01:01:52 +0200

    Update delet_fromtable.sh

--- bash_project/delet_fromtable.sh
+++ bash_project/delet_fromtable.sh
@@ -1,8 +1,8 @@
-awk ''
-
-echo "Enter the name of table u want to delet from  it: "
-		read table
-		echo "enter the column value"
-		read value
-sed  '/$value/d' ./RDBMS/database/$data_base/$table/data
-
+echo "Enter the name of table u want to delete from: "
+read table
+echo "Enter the name u want to delete: "
+read name
+#awk -F, '{print $1"\t"$2"\t"$3}' ./Header
+#(awk -F, -vX="$name" '{if( $2 == X){print $1"\t"$2"\t"$3}}' ./data )|(sed -i "d" ./data)
+sed -i '/'$name'/d' ./RDBMS/database/$data_base/$tablename/data
+echo 'Row deleted'
